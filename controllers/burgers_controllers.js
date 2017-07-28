2b32bb21e9cd41a8ffda658872ca30d8b04e4308
Prabhdeep <ps591990@gmail.com>
2017-07-27 21:45:10 -0500

    update routes and orm

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,34 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var burgerObject = {
+            burgers: data
+        };
+        console.log(burgerObject)
+        res.render("index", burgerObject);
+    })
+
+})
+
+router.post("/", function(req, res) {
+    burger.create(["burger_name"], [req.body.name], function(data){
+        res.redirect("/")
+    })
+})
+
+router.put("/:id", function(req, res) {
+    var condition = "id = "+req.params.id;
+
+    burger.update(condition, function(){
+        res.redirect("/")
+    })
+})
+
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,24 @@
+var orm = require("../config/orm.js");
+
+
+
+var burgers = {
+    all: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res)
+        });
+
+    },
+    create: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res){
+            cb(res)
+        });
+    },
+    update: function(condition, cb) {
+        orm.updateOne("burgers", "devoured = true", condition, function(res){
+            cb(res)
+        })
+    }
+ }
+
+ module.exports = burgers;
